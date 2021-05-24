cc82f3d87b23c71cdd4a64938ba31d6d476fd06a
shareehatch <>
2021-05-24 03:14:20 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const input = require('readline-sync');
+
+let num1 = input.question("Enter a number: ");
+let num2 = input.question("Enter another number: ");
+
+console.log(Number(num1) + Number(num2));
+
+
+
+
+
